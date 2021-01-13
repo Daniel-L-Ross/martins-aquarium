87294211e86b0a69b88e1caa20b5d9c0dfed8ef4
Dan Ross <daniellross@gmail.com>
2021-01-13 11:49:24 -0600

    deleted file that was duplicated by accident. fixed module paths in js files

--- scripts/fish/FishList.js
+++ scripts/fish/FishList.js
@@ -1,5 +1,5 @@
-import { useFish } from './FishDataProvider.js'
-import { Fish } from './Fish.js'
+import { useFish } from '/scripts/fish/FishDataProvider.js'
+import { Fish } from '/scripts/fish/Fish.js'
 
 // for of loop to populate the dom with the fish
 export const FishList = () => {
