891f81ba402eb993dbd0f2bc1f421366b2d178a2
Dan Ross <daniellross@gmail.com>
2021-01-13 13:14:40 -0600

    added export function for single fish

--- scripts/tips/TipDataProvider.js
+++ scripts/tips/TipDataProvider.js
@@ -1,11 +1,11 @@
 const tipCollection = [
     {
         subject: "Feeding Fish",
-        tipText: "The time of day you feed your fish is important. Try to do it at the same time every morning to help your fish feel safe."
+        text: "The time of day you feed your fish is important. Try to do it at the same time every morning to help your fish feel safe."
     },
     {
         subject: "Fish Exercise",
-        tipText: "Avoid taking your fish for walks, as they can't breathe air and don't have legs."
+        text: "Avoid taking your fish for walks, as they can't breathe air and don't have legs."
     }
 ]
 
