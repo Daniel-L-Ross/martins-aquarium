17268722a883ad0b7555a348977eb634e3c36ff2
Dan Ross <daniellross@gmail.com>
2021-01-12 18:02:34 -0600

    added additional properties to fish. added export line

--- scripts/FishDataProvider.js
+++ scripts/FishDataProvider.js
@@ -1,6 +1,13 @@
 const fishCollection = [
     {
         name: Bart,
-        food: crustaceans
+        species: Clownfish,
+        location: Great Barrier Reef,
+        length: "4 inches",
+        food: crustaceans,
     }
 ]
+
+export const useFish = () => {
+    return fishCollection.slice()
+}
