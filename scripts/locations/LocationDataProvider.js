9ac99d4359a8e3ace9300e4985e2af9aca8d780e
Dan Ross <daniellross@gmail.com>
2021-01-13 16:46:51 -0600

    created files and location list object

---
+++ scripts/locations/LocationDataProvider.js
@@ -0,0 +1,9 @@
+const locationList = [
+    {
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/PALM_BEACH_FLORIDA_AERIAL_2011.jpg/250px-PALM_BEACH_FLORIDA_AERIAL_2011.jpg",
+        name: "Gulf Coast",
+        city: "Palm Beach",
+        state: "Florida",
+        country: "Unite States",
+    },
+]
