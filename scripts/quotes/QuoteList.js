d6b3065f6e28624579c9471c9a1d0c83e629f264
Dan Ross <daniellross@gmail.com>
2021-01-14 10:07:03 -0600

    minor change to test correcting github issue

--- scripts/quotes/QuoteList.js
+++ scripts/quotes/QuoteList.js
@@ -1,11 +1,1 @@
-export const Quote = (quote) => {
-    return `
-    <div class ="quoteCard">
-        <div class="quoteCard__content">
-            <div class="quoteCard__content-text">${quote.text}</div>
-            <div class="quoteCard__content-author">${quote.author}</div>
-            <div class="quoteCard__content-location">${quote.location}</div>
-        </div>
-    </div>
-    `
-}
+ 
