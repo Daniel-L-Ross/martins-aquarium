aeb89eea53ee558c5ff7d7ebb12f634701efda07
Dan Ross <daniellross@gmail.com>
2021-01-14 14:00:34 -0600

    additional adjustments to the automated html

--- scripts/tips/Tip.js
+++ scripts/tips/Tip.js
@@ -1,7 +1,7 @@
 export const Tip = (tip) => {
     return `
         <div class="tip__card">
-            <div class="tip__card-subject">${tip.subject}</div>
+            <div class="tip__card-subject">Subject: ${tip.subject}</div>
             <div class="tip__card-text">${tip.text}</div>
         </div>
     `
