59b19844a7b036c93b7c0ce5624c664a0866ee97
Dan Ross <daniellross@gmail.com>
2021-01-15 13:59:44 -0600

    made fishlist more dry

--- scripts/fish/FishList.js
+++ scripts/fish/FishList.js
@@ -1,7 +1,15 @@
-import { MostHolyFish } from '/scripts/fish/FishDataProvider.js'
-import { SoldierFish } from '/scripts/fish/FishDataProvider.js'
-import { UnHolyFish } from '/scripts/fish/FishDataProvider.js'
+import { MostHolyFish, SoldierFish, UnHolyFish } from '/scripts/fish/FishDataProvider.js'
 import { Fish } from '/scripts/fish/Fish.js'
+
+// new funcion to automate repetitve code
+const buildFishHTML = (array, heading) => {
+    let fishHTML = `<h3 class="fish__title">${heading}</h3><div class="contentArticle">`;
+    for (const fish of array) {
+        fishHTML += Fish(fish)
+    }
+    fishHTML +=`</div>`;
+    return fishHTML
+}
 
 // for of loop to populate the dom with the fish
 export const FishList = () => {
@@ -9,41 +17,16 @@ export const FishList = () => {
     const holyFishes = MostHolyFish()
     const soldierFishes = SoldierFish()
     const unholyFishes = UnHolyFish()
-    let holyFishHTML = ""
-    let soldierFishHTML = ""
-    let unholyFishHTML = ""
-    
-    // for of loop to get holy fish
-    for (const fish of holyFishes) {
-        holyFishHTML += Fish(fish);
-    }
-    
-    // for of loop to get holy fish
-    for (const fish of soldierFishes) {
-        soldierFishHTML += Fish(fish);
-    }
-    
-    // for of loop to get holy fish
-    for (const fish of unholyFishes) {
-        unholyFishHTML += Fish(fish);
-    }    
+    let holyFishHTML = buildFishHTML(holyFishes, 'Holy Fish');
+    let soldierFishHTML = buildFishHTML(soldierFishes, 'Soldier Fish')
+    let unholyFishHTML = buildFishHTML(unholyFishes, 'Other Fish')
 
     contentElement.innerHTML += `
         <h2 class="content__title">Martin's Fish List</h2>
         <article class="containerLeft__fishList">
-            <h3 class="fish__title">Holy Fish</h3>
-            <div class="contentArticle">
                 ${holyFishHTML}
-            </div>
-            <h3>Soldier Fish</h3>
-            <div class="contentArticle">
                 ${soldierFishHTML}
-            </div>
-            <h3>Other Fish </h3>
-            <div class="contentArticle">
                 ${unholyFishHTML}
-            </div>
         </article>
     `
 }
-
